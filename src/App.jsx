978f6bcaa95fe6e4f4750975d84839cf06156bc7
Eddhan Tan <eddhan12345@gmail.com>
2026-02-16 20:11:59 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-blue-200 min-h-dvh flex items-center justify-center'>
+      <h1 className='text-3xl font-bold text-blue-800'>Student Planner App</h1>
+    </div>
+  )
+}
+
+export default App
